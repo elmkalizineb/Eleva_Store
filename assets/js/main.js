648b93038ed6f6ecf44ff4306277813b0525e7b2
elmkalizineb <elmkalizineb@gmail.com>
2026-04-03 12:28:34 +0100

    Products Tabs JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,10 +36,23 @@ var swiperCategories = new Swiper('.categories__container', {
 
 /*=============== PRODUCTS TABS ===============*/
 const tabs = document.querySelectorAll('[data-target]'),
-    tabContent = document.querySelectorAll('[content]');
+    tabContents = document.querySelectorAll('[content]');
 
-    tabs.forEach((tab) =>{
-        tab.addEventListener('click',()=>{
-            
-        })
-    })
+// console.log(tabContents);
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target);
+        tabContents.forEach((tabContent) => {
+            tabContent.classList.remove('active-tab');
+        });
+
+        target.classList.add('active-tab');
+
+        tabs.forEach((tab) => {
+            tab.classList.remove('active-tab');
+        });
+
+        tab.classList.add('active-tab');
+    });
+});
